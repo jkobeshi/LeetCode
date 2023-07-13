c0ad3d65d01390afe6e2102ec587b24a97e1765d
Kobe Shi <112596856+jkobeshi@users.noreply.github.com>
2023-07-13 15:57:17 -0400

    Time: 45 ms (41.94%), Space: 21.6 MB (24.81%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> m;
+        for(int i = 0; i < strs.size(); ++i){
+            string set = "00000000000000000000000000";
+            for(int j = 0; j < strs[i].size(); ++j)
+                set[strs[i][j] - 'a']++;
+            m[set].push_back(strs[i]);
+        }
+        vector<vector<string>> res;
+        for(auto it = m.begin(); it != m.end(); ++it)
+            res.push_back(it->second);
+        return res;
+    }
+};
