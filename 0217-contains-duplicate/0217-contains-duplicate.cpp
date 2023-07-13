f6fdd0281750ce393ef1dd3a462d451a154cf70e
Kobe Shi <112596856+jkobeshi@users.noreply.github.com>
2023-07-13 01:05:18 -0400

    Time: 161 ms (40.64%), Space: 69.4 MB (60.08%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> noDup;
+        noDup.insert(nums[0]);
+        for(int i = 1; i < nums.size(); ++i){
+            if(noDup.find(nums[i]) != noDup.end()) return true;
+            noDup.insert(nums[i]);
+        }
+        return false;
+    }
+};
