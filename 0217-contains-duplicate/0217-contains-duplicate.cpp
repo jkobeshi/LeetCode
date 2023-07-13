1017fd0243159149521d58b9feda1a551738fedf
Kobe Shi <112596856+jkobeshi@users.noreply.github.com>
2023-07-13 01:17:31 -0400

    Time: 138 ms (81.88%), Space: 57.2 MB (93.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int maxx = nums[0];
+        sort(nums.begin(), nums.end());
         for(int i = 1; i < nums.size(); ++i)
-            maxx = max(maxx, abs(nums[i]));
-        vector<bool> set((maxx + 1) * 2);
-        for(int i = 0; i < nums.size(); ++i){
-            int val = nums[i];
-            if(val < 0) val = maxx + abs(val);
-            if(set[val]) return 1;
-            set[val] = 1;
-        }
-        return 0;
+            if(nums[i] == nums[i - 1]) return true;
+        return false;
     }
 };
