ae205ff33bc185a78d9d47cc92c4d64a38e009cb
Kobe Shi <112596856+jkobeshi@users.noreply.github.com>
2023-07-13 15:51:03 -0400

    Time: 12 ms (80.92%), Space: 10.8 MB (31.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> m;
-        m[target - nums[0]] = 0;
-        for(int i = 1; i < nums.size(); ++i){
-            if(m.find(nums[i]) != m.end()) return {m[nums[i]], i};
-            m[target - nums[i]] = i;
+        unordered_map<int, int> firToSec;
+        firToSec[nums[0]] = 0;
+        for (int i = 1; i < nums.size(); ++i){
+            auto it = firToSec.find(target - nums[i]);
+            if (it != firToSec.end()){
+                return {i, it->second};
+            }
+            firToSec[nums[i]] = i;
+
+        }
+        for (int i = 1; i < nums.size(); ++i){
+            auto it = firToSec.find(target - nums[i]);
+            if (it != firToSec.end() && i != it->second){
+                return {i, it->second};
+            }
         }
         return {};
     }
