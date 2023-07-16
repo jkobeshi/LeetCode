dbe21f467fd00e0fc82bebbb58ed907561eacb3c
Kobe Shi <112596856+jkobeshi@users.noreply.github.com>
2023-07-16 13:41:28 -0400

    Time: 38 ms (10.61%), Space: 19.9 MB (34.69%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> st;
+        for(int i = 0; i < board.size(); ++i){
+            for(int j = 0; j < board[0].size(); ++j){
+                if(board[i][j] != '.'){
+                    string r = to_string(board[i][j]) + "row" + to_string(i);
+                    string c = to_string(board[i][j]) + "col" + to_string(j);
+                    string b = to_string(board[i][j]) + "box" + to_string((j / 3)) + to_string((i/3));
+                    if(st.find(r) != st.end() || 
+                        st.find(c) != st.end() ||
+                        st.find(b) != st.end()) return false;
+                    st.insert(r);
+                    st.insert(c);
+                    st.insert(b);
+                }
+            }
+        } return true;
+    }
+};
