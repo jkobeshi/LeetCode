f4263d9e302f93d1fccf42597aee2e0fed0246fa
Kobe Shi <112596856+jkobeshi@users.noreply.github.com>
2023-08-07 22:04:50 -0400

    Time: 4 ms (82.33%), Space: 11 MB (47.93%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-
-    vector<vector<int>> result;
+    
+    vector<vector<int>> res;
     int tar;
+    
     void dfs(vector<int>& candidates, int sum, int start, vector<int>& curr) {
         if (sum > tar) {
             return;
         }
         if (sum == tar) {
-            result.push_back(curr);
+            res.push_back(curr);
             return;
         }
         for (int i = start; i < candidates.size(); i++) {
@@ -17,13 +18,13 @@ public:
             curr.pop_back();
         }
     }
-
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         tar = target;
-        vector<int> curr;
-        dfs(candidates, 0, 0, curr); 
-        return result;
+        vector<int> temp;
+        dfs(candidates, 0, 0, temp);
+        return res;
     }
      
 };
