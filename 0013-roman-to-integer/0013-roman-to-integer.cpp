df6b642e2854d1178c4b63ca2e111f4b8dec3b2d
Kobe Shi <112596856+jkobeshi@users.noreply.github.com>
2023-10-24 11:53:44 -0400

    Time: 6 ms (80.86%), Space: 6.3 MB (89.79%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int LtoI(char c){
+        switch(c){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return 0;
+    }
+    int romanToInt(string s) {
+        int pre = LtoI(s[0]);
+        int res = pre;
+        for(int i = 1; i < s.size(); ++i){
+            int cur = LtoI(s[i]);
+            if (cur <= pre){
+                res += cur;
+                pre = cur;
+            }
+            else{
+                res = res - pre + (cur - pre);
+                pre = cur;
+            }
+        }
+        return res;
+    }
+};
