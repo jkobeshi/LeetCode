b218f5bc70c683437c0b9d4447952182b6a25a55
Kobe Shi <112596856+jkobeshi@users.noreply.github.com>
2023-10-25 13:07:36 -0400

    Time: 0 ms (100.00%), Space: 6.9 MB (36.63%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int cnt = 0;
         int i = s.size() - 1;
         while(i >= 0 && s[i] == ' ') --i;
         int endInd = i;
