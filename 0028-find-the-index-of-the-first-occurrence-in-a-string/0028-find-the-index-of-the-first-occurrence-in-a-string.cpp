a4156fe1c19fc3f73bde2845e52a55e695a94f6f
Kobe Shi <112596856+jkobeshi@users.noreply.github.com>
2023-10-25 12:41:05 -0400

    Time: 4 ms (43.15%), Space: 6.8 MB (16.83%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         for(int i = 0; i < haystack.size(); ++i){
-            if(haystack.substr(i, needle.size()) == needle){
+            if(haystack.substr(i, needle.size()) == needle)
                 return i;
-            }
         }
         return -1;
     }
